3bf9d744674c832f86cc9e2a9dedc94d9a95da14
dorvake <zesebua30@gmail.com>
2024-11-27 12:29:59 +0400

    first commit

---
+++ src/main/java/org/paralleldv/alloymod/PeriodityClient.java
@@ -0,0 +1,11 @@
+package org.paralleldv.alloymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PeriodityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/paralleldv/alloymod/PeriodityDataGenerator.java
@@ -0,0 +1,11 @@
+package org.paralleldv.alloymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PeriodityDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
